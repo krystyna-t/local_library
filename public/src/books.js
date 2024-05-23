ba25c052206561f1a000a04f476d13b01854a2ab
krystyna <krystyna@indeed.com>
2024-05-23 21:36:31 +0900

    Initial commit of local library dashboard

---
+++ public/src/books.js
@@ -0,0 +1,48 @@
+function findAuthorById(authors, id) {
+  // Returns the author object that has the matching ID
+  return authors.find((author) => author.id === id);
+}
+
+function findBookById(books, id) {
+  // Returns the book object that has the matching ID
+  return books.find((book) => book.id === id);
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function partitionBooksByBorrowedStatus(books) {
+  return books.reduce(
+    (acc, book) => {
+      const [borrowed, returned] = acc;
+      const recent = book.borrows[0];
+      if (recent.returned) {
+        returned.push(book);
+      } else {
+        borrowed.push(book);
+      }
+
+      return acc;
+    },
+    [[], []]
+  );
+}
+
+function getBorrowersForBook(book, accounts) {
+  const accountsById = accounts.reduce((acc, account) => {
+    acc[account.id] = account;
+    return acc;
+  }, {});
+
+  return book.borrows
+    .map(({ id, returned }) => ({
+      ...accountsById[id],
+      returned,
+    }))
+    .slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
